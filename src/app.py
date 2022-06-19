7088844cbddd2803ae3cd64aeef77a060b7fe51e
FloCorvalan <florencia.corvalan@usach.cl>
2022-06-19 15:56:18 -0400

    fix env variables

--- src/app.py
+++ src/app.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv
+from os import environ, getcwd
 from apps import create_app
 
+
 if __name__ == "__main__":
-    app, mongo = create_app('config.DevelopmentConfig')
-    app.run(port=5002)
+    pwd = getcwd()
+    dotenv_path = pwd + '/.env'
+    load_dotenv(dotenv_path)
+    config_mode = environ.get('CONFIG_MODE')
+    app_port = environ.get('PORT')
+    app, mongo = create_app(config_mode)
+    app.run(port=app_port)
--- src/config.py
+++ src/config.py
@@ -4,7 +4,6 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = '57e19ea558d4967a552d03deece34a70'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
@@ -15,12 +14,11 @@ class DevelopmentConfig(Config):
     ENV="development"
     DEVELOPMENT=True
     DEBUG=True
-    MONGO_URI="mongodb://admin:adm1n!@localhost:27018/prueba"
+    MONGO_URI=os.environ.get('DATABASE_DEV_URL')
 
 class TestConfig(Config):
     ENV="test"
     TESTING=True
     DEVELOPMENT=False
     DEBUG=False
-    MONGO_URI="mongodb://localhost:27018/test"
-    
+    MONGO_URI=os.environ.get('DATABASE_TEST_URL')
