9cabcd402ef6aa06d1b8d2036679862f7214d881
FloCorvalan <florencia.corvalan@usach.cl>
2022-06-25 14:59:37 -0400

    csrf r

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = False
+    CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
